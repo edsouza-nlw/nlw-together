c69831626df9e555685bfadfe25025c72d099c84
edrajao <4ucode@gmail.com>
2021-06-20 22:42:49 -0300

    Firebase Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-function App() {
+import { Button } from './components/Button'
+
+export default function App() {
   return (
-  <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <Button />
+      <Button />
+      <Button />
+    </div>
+
   );
 }
 
-export default App;
